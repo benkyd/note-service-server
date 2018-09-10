846fa32059610846a5c6df3e195a9466dc663f6d
Ben <playsplane@gmail.com>
2018-09-10 14:03:34 +0100

    idk

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -8,8 +8,24 @@ export class NoteController extends ControllerHandler {
     static async newNote(req, res, next) {
         Logger.debug(JSON.stringify(req.user, false, 4));
 
-        console.dir(__stack, { depth: null })
+        let content = req.body.text || undefined;
+        let creatorid = req.user.id || undefined;
+        let group = req.body.parentgroup || undefined;
+        let order = req.body.order || undefined;
+
         
+
         next();
     }
 }
+
+// id: id,
+// content: content,
+// parentgroup: parentgroup,
+// creatorid: creatorid,
+// order: order,
+// catergory: null,
+// endpoint: null,
+// lastupdated: new Date().getTime()
+
+// static async newNote(id, content, creatorid, order, parentgroup) {
