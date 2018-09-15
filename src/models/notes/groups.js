f9a93d61ab5e4cf74058ecef3cb0f480574998fa
Ben <playsplane@gmail.com>
2018-09-15 11:21:58 +0100

    started note abstractions

--- src/models/notes/groups.js
+++ src/models/notes/groups.js
@@ -1,3 +1,3 @@
 export class Groups {
-
+    constructor() { }
 }
--- src/models/notes/notes.js
+++ src/models/notes/notes.js
@@ -1,5 +1,32 @@
 import {Groups} from './groups';
+import {Database} from '../database/database';
 
 export class Notes extends Groups {
+    static async newNote(id, content, creatorid, order) {
+        Database.notes.newNote(id, null, )
+    }
 
+    static async getNoteByEndpoint(endpoint) {
+        
+    }
+
+    static async makeNotePerma(id, endpoint) {
+
+    } 
+
+    static async newGroupedNote(id, content, creatorid, order, parentgroup) {
+
+    }
+
+    static async reorderNote(id, newPosition) {
+
+    }
+
+    static async regroupNote(id, groupid) {
+
+    }
+    
+    static async deleteNote(id) {
+
+    }
 }
